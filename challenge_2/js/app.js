865d938699c0331d541e0c5aa9f74e25c3267e6c
Mark Johnson <markjohnson303@gmail.com>
2016-08-29 07:29:24 -0600

    complete

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,27 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+function WelcomeController(){
+  this.full_name = "Mark Johnson"; // change me
+  this.age = 28;
+  this.city = "Littleton";
+  this.state = "CO";
+  this.shout = function(str) {
+  	return str + "!";
+  };
+}
+
+
+app.controller("ClassController", ClassController);
+
+function ClassController(){
+  this.class_name = "White Walkers"; // change me
+  this.enrolled_students = 14;
+  this.start_date = "June";
+  this.end_date = "September";
+  this.shout = function(str) {
+  	return str + "!";
+  };
+}
